c5e68059094e5eb798e7b17090dfae9e75518b64
Mach Kobayashi <machwerx@gmail.com>
2024-10-05 21:45:51 -0700

    critters flock towards each other

--- Assets/Scripts/Critter.cs
+++ Assets/Scripts/Critter.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Critter : MonoBehaviour {
+    private float kDistanceGoal = 0.15f;
+
     // Start is called before the first frame update
     void Start() {
 
@@ -11,6 +13,23 @@ public class Critter : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         float speed = 1.0f;
+        Vector3 targetPos = 0 * transform.position;
+        float weight = 0 * 1.0f;
+        foreach (Transform critter in transform.parent) {
+            if (!critter.gameObject.activeSelf || critter == this.transform) {
+                continue;
+            }
+
+            float w = 1.0f / (0.05f + (critter.transform.position - transform.position).magnitude);
+            Vector3 target = (transform.position - critter.position).normalized * kDistanceGoal + critter.position;
+
+            targetPos += w * target;
+            weight += w;
+        }
+        targetPos /= weight;
+        transform.rotation = Quaternion.FromToRotation(Vector3.forward, targetPos - transform.position);
+
         transform.position += speed * Time.deltaTime * (transform.rotation * Vector3.forward);
+        //transform.position = 0.9f * transform.position + 0.1f * targetPos;
     }
 }
